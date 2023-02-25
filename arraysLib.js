61ca1a6363d45225351bfc6ee3a8e24544d6cf46
GTedZ <Hellis_over@icloud.com>
2023-02-25 04:57:53 +0200

    initial commit

--- arraysLib.js
+++ arraysLib.js
@@ -1,11 +1,12 @@
 let api = function everything() {
     if (this.target == 'undefined') return new everything();
 
-    const arrayEquality = (...arrs) => {
+    const arrayEquality = (stopOnException, ...arrs) => {
+        let non_included_elements = new Set();
         if (arrs.length <= 1) return true;
         const length = arrs[0].length;
 
-        if (!arrs.every(arr => arr.length == length)) return false;
+        if (!arrs.every(arr => arr.length == length) && stopOnException) return false;
 
         for (let main_array_index = 0; main_array_index < arrs.length; main_array_index++) {
             const main_array = arrs[main_array_index];
@@ -15,21 +16,34 @@ let api = function everything() {
                 for (let secondary_array_index = 0; secondary_array_index < arrs.length; secondary_array_index++) {
                     if (secondary_array_index == main_array_index) continue;
                     const secondary_array = arrs[secondary_array_index];
-                    if (!elementIncluded(element, secondary_array)) return false;
+                    if (!elementIncluded(element, secondary_array)) {
+                        if (stopOnException) return false;
+                        else handleAddElementToSet(element, non_included_elements)
+                    }
                 }
 
             }
         }
 
-        return true;
+        if (stopOnException) return true;
+        else {
+            if (non_included_elements.size == 0) return false;
+            else return Array.from(non_included_elements);
+        }
+
+        function handleAddElementToSet(element, set) {
+            if (typeof element == 'object') return handleObject(element, array);
+            else if (typeof element == 'function') return handleFunction(element, array);
+            else return set.add(element);
+        }
     }
 
     this.arrayEquality = (...arrs) => {
-        return arrayEquality(...arrs);
+        return arrayEquality(true, ...arrs);
     }
 
     this.findDifference = (...arrs) => {
-
+        return arrayEquality(false, ...arrs);
     }
 
     // helper functions
--- test.js
+++ test.js
@@ -1,8 +1,8 @@
 const arraysLib = require('./arraysLib');
 
-const arr1 = [1, 2, 3, 4, 5, 6];
-const arr2 = [1, 2, 3, 4, 6, 5];
+const arr1 = [1, 2, 3, 4, 5, 6, 8];
+const arr2 = [1, 2, 3, 4, 6, 6, 7];
 
-const x = arraysLib.arrayEquality(arr1, arr2);
+const x = arraysLib.findDifference(arr1, arr2);
 
 console.log(x);
